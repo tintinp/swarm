6d5b02ad139f97f4c2ce76ddc651eaa9c1a18c98
tintinp <tinpatana@gmail.com>
2020-02-22 18:01:14 -0800

    add express server

---
+++ flight-visualizer-js/server/app.js
@@ -0,0 +1,31 @@
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error', { error: err })
+})
+
+module.exports = app
